05b82b08a047fe3a555948ea72c579da8dac1d5c
eef <uberhaqer@gmail.com>
2012-09-24 12:27:21 +0100

    cleaned up devise generated comments from the user model and added easy roles to the model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,9 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  easy_roles :roles
 
-  # Setup accessible (or protected) attributes for your model
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+
   has_many :orders
 end
