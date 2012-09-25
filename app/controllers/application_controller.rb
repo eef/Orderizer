00390279c7179d54d9eec62decd68f98d6e4d812
eef <uberhaqer@gmail.com>
2012-09-25 15:45:00 +0100

    added some comments to the application_controller.rb to explain about filters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  # filters are methods provided by rails.  you get controller filters and model filters.
+  # a before_filter placed in the application controller will run before an action is called
+  # in controllers.  'authenticate_user' is a dynamic method provided by the devise gem.
+  # You can run your own custom method before all other actions.  Commented out example below.
   before_filter :authenticate_user!
+  # before_filter :custom_method
+  
+  
+  # def custom_method
+  #   if something.eql?(something_else)
+  #     do_something
+  #   end
+  # end
 
 end
