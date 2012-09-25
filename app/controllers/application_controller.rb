c30c8ef8effa9b209eb7bbda2c8049355e24ac75
eef <uberhaqer@gmail.com>
2012-09-25 15:46:00 +0100

    added a link to the filters documentation page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
   # a before_filter placed in the application controller will run before an action is called
   # in controllers.  'authenticate_user' is a dynamic method provided by the devise gem.
   # You can run your own custom method before all other actions.  Commented out example below.
+  # A link to the documentation on filters and the different ones available
+  # http://rails.rubyonrails.org/classes/ActionController/Filters/ClassMethods.html
   before_filter :authenticate_user!
   # before_filter :custom_method
   
