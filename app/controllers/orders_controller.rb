e121c8977851d30bdafb736289d31a0d1b9ffa0a
eef <uberhaqer@gmail.com>
2012-10-02 22:05:20 +0100

    added class methods to order model for open and closed orders, moved the order actions to partial and only admins can edit or delete and order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,8 +4,7 @@ class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.json
   def index
-    @orders = Order.all
-
+    @orders = {:open => Order.open, :closed => Order.closed}
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @orders }
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,13 @@ class Order < ActiveRecord::Base
   attr_accessible :status
 
   belongs_to :user
+
+  class << self
+    def open
+      where(:status => "open")
+    end
+    def closed
+      where(:status => "closed")
+    end
+  end
 end
