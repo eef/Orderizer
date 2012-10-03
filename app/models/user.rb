1849993645ec0e1e27f2ad248018a7dd0d2f6d63
eef <uberhaqer@gmail.com>
2012-10-03 17:07:10 +0100

    added instance method to return the users full name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,12 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
 
   has_many :orders
+
+  def name
+    [self.first_name, self.last_name].join(" ")
+  end
+
 end
