5feb831b8276c64e01210539b3a62670fae90274
eef <uberhaqer@gmail.com>
2012-10-04 13:14:21 +0100

    added mailer to mail user order confirmation and link to edit their item

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -7,6 +7,7 @@ class LineItemsController < ApplicationController
     @order.line_items << @line_item
     respond_to do |format|
       if @order.save
+        OrderMailer.added_line_item(@line_item).deliver
         format.html { redirect_to @order, notice: 'Your roll has been added to the order cunt face.' }
         format.json { render json: @order, status: :created, location: @order }
       else
@@ -15,5 +16,25 @@ class LineItemsController < ApplicationController
       end
     end
   end
+
+  def edit
+    @line_item = current_user.line_items.find(params[:id])
+    @order = @line_item.order
+  end
+
+  def update
+    @order = Order.find(params[:order_id])
+    @line_item = current_user.line_items.find(params[:id])
+    respond_to do |format|
+      if @line_item.update_attributes(params[:line_item])
+        format.html { redirect_to @order, notice: 'You roll was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 
 end
--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -6,4 +6,9 @@ class OrderMailer < ActionMailer::Base
     @order = order
     mail(:subject => "A new roll order is open!")
   end
+  
+  def added_line_item(item)
+    @item = item
+    mail(:subject => "Your roll has been added to the order!")
+  end
 end
