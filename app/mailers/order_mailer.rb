2aac84259c0aa7eaba602ec98e0d0895b73c8893
eef <uberhaqer@gmail.com>
2012-10-04 10:39:21 +0100

    tenth time lucky with the mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -3,6 +3,7 @@ class OrderMailer < ActionMailer::Base
           :from => "orders@orderizer.herokuapp.com"
 
   def order_open(order)
+    @order = order
     mail(:subject => "A new roll order is open!")
   end
 end
