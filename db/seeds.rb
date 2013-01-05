0577462a42a38db98b0052fe26b360d24589c4c0
Will McGregor <william.mcgregor@gmail.com>
2013-01-05 16:03:27 +0000

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 User.destroy_all
 Menu.destroy_all # Items attached to this menu will automatically be destroy because :dependent => :destroy exists on the association in menu.rb line 3
 Order.destroy_all
-
+#
 # Create the users
 admin_1 = User.create! :email => "william.mcgregor@gmail.com", :password => "bawheed123", :password_confirmation => "bawheed123", :first_name => "Will", :last_name => "McGregor"
 admin_2 = User.create! :email => "uberhaqer@gmail.com", :password => "yermaw123", :password_confirmation => "yermaw123", :first_name => "Arthur", :last_name => "Canal"
